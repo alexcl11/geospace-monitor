6947ddc20b7ef9d95fd0f8b402a9a98e0398d1a4
alexcl11 <aleecanovaslopez@gmail.com>
2026-03-25 22:32:02 +0100

    fix: url definitiva con https y duckdns

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://4a3c-2a01-4f8-1c19-36af-00-1.ngrok-free.app/api/events?limit=3000',
+  apiUrl: 'https://geospace-alejandro-canovas.duckdns.org/api/events?limit=3000'
 };
