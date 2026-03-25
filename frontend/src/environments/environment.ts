78cbfcadde30be1ed81ed9b96064fcf883dbde4e
alexcl11 <aleecanovaslopez@gmail.com>
2026-03-25 21:18:53 +0100

    fix: url de ngrok con limite de 3000

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://138.199.202.214:8000/api/events?limit=5000'
+  apiUrl: 'https://4a3c-2a01-4f8-1c19-36af-00-1.ngrok-free.app?limit=3000',
 };
